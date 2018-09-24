916c2a2f23254154a044ed67961f026e0c2daa07
Daniel Christophe <dcchrist@iu.edu>
2018-09-24 09:32:03 -0400

    Initial commit

---
+++ public/js/websiteApp.js
@@ -0,0 +1,85 @@
+var websiteApp = new Vue({
+  el: '#hello',
+  data: {
+    results: [
+      {
+        gender: '',
+        name: {
+          title: '',
+          first: '',
+          last: ''
+          },
+        location: {
+          street: '',
+          city: '',
+          state: '',
+          postcode: '',
+          },
+        email:'',
+        dob: {
+          date: '',
+          age: '',
+          },
+        phone: '',
+        picture:{
+            large: "",
+            medium: "",
+            thumbnail: ""
+         },
+      }
+    ]
+  },
+
+  methods: {
+      fetchResults () {
+        fetch('https://randomuser.me/api/?format=json')
+        .then( response => response.json() )
+        .then( json => {websiteApp.results = json.results[0];
+        } )
+        .catch( err => {
+          console.log('RESULTS FETCH ERROR:');
+          console.log(err);
+          })
+        },
+
+        agenumber: function(){
+          const xy= moment(this.results.dob.date).diff(moment(), 'years');
+          return Math.abs(xy);
+        },
+      pretty_date: function (d) {
+        return moment(d).format('l')
+      },
+        refresh: function(event){
+          this.fetchResults();
+        }
+      },
+  created () {
+    this.fetchResults();
+
+    }
+  })
+
+
+//from dashboard for reference
+
+// completeClass: function(task) {
+//   if (task.perc_complete == 100 ) {
+//     return 'alert-success'
+//   }
+//   if (task.current_sprint && task.hours_worked == 0 ) {
+//     return 'alert-warning'
+//   }
+// },
+
+  //   pretty_currency: function (val) {
+  //   if (val < 1e3) {
+  //     return '$ ' + val
+  //   }
+  //   if (val < 1e6) {
+  //     return '$ ' + (val/1e3).toFixed(1) + ' k'
+  //   }
+  //
+  //   return '$ ' + (val/1e6).toFixed(1) + ' M'
+  // },
+  //
+  // computed: {
