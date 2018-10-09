30e3d5819c89ed6785b5e2898cfbd4655d123811
Daniel Christophe <42592064+danielchristophe95@users.noreply.github.com>
2018-10-08 22:20:52 -0400

    test

--- public/api/work.php
+++ public/api/work.php
@@ -5,14 +5,14 @@ require '../../app/common.php';
 $id = intval($_GET['id'] ?? 0);
 
 if ($id < 1) {
-  throw new Exception('Invalid IDD');
+  throw new Exception('Invalid ID');
 }
 
 $workArr = Work::getAllWork($id);
 
 $json = json_encode($workArr);
 
-echo $json;
+echo hello;
 //go to db to get work associated with ID
 // convert to JSON
 //print
