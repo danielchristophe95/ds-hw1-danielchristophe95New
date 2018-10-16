96fbadafce3dfd4340057571c648c5e1f096410f
Daniel Christophe <42592064+danielchristophe95@users.noreply.github.com>
2018-10-16 13:56:45 -0400

    finishing touches

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -22,7 +22,7 @@ public static function fetchAll() {
   $arr = [];
   while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
 
-    $workItem = new Comment($row);
+    $workItem = new Work($row);
 
     array_push($arr, $workItem);
   }
